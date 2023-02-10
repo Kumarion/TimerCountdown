a6dc2b3d96e4d2183ac7d2ce683662fbd84a0264
Kumarion <121711454+Kumarion@users.noreply.github.com>
2023-02-10 15:10:36 -0500

    Fixed readme

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# TimerCountdown
-A wrapper for Sleitnick's Timer. That allows you to create a countdown and a timer with tracking to disconnect timers/countdowns.
+<div align="center">
+
+# Status:
+Main code lint status: 
+[![Lint](https://github.com/Kumarion/Repli/actions/workflows/lint.yaml/badge.svg)](https://github.com/Kumarion/Repli/actions/workflows/lint.yaml)
+
+Documentation build status: 
+[![Documentation](https://github.com/Kumarion/Repli/actions/workflows/docs.yaml/badge.svg)](https://github.com/Kumarion/Repli/actions/workflows/docs.yaml)
+
+Documentation publish status: 
+[![Published](https://github.com/Kumarion/Repli/actions/workflows/pages/pages-build-deployment/badge.svg)](https://github.com/Kumarion/Repli/actions/workflows/pages/pages-build-deployment)
+
+</div>
--- lib/TimerCountdown/init.lua
+++ lib/TimerCountdown/init.lua
@@ -3,17 +3,15 @@ local Signal = require(script.Signal);
 local SleitnickTimer = require(script.Timer);
 
 --[=[
-	@class TimerCountdown
-
     The TimerCountdown class that returns a new TimerCountdown object with functions to create timers and countdowns.
+    @class TimerCountdown
 ]=]
 local TimerCountdown = {};
 TimerCountdown.__index = TimerCountdown;
 
 --[=[
-	@return TimerCountdown
-	
-	Creates a new timer object.
+    Creates a new timer object.
+    @return TimerCountdown
 ]=]
 function TimerCountdown.new()
     local self = setmetatable({}, TimerCountdown);
@@ -22,19 +20,17 @@ function TimerCountdown.new()
 end
 
 --[=[
-	@param countdownName string
+    Uses Sleitnicks Timer module to create a ``Simple`` timer that counts down from ``start`` to 0.
+    Returns a signal that fires when the countdown is finished.
+
+    If ``stopAtZero`` is true, the countdown will stop at 0. If it is false, the countdown will stop at 1.
+    Countdown is automatically disconnected at the end of the countdown.
+
+    @param countdownName string
     @param start number
     @param interval number
     @param updateFn function
     @param stopAtZero boolean
-
-	Uses Sleitnicks Timer module to create a ``Simple`` timer that counts down from ``start`` to 0.
-    Returns a signal that fires when the countdown is finished.
-
-    If ``stopAtZero`` is true, the countdown will stop at 0. If it is false, the countdown will stop at 1.
-
-    Countdown is automatically disconnected at the end of the countdown.
-
     @return Signal
 ]=]
 function TimerCountdown:Countdown(countdownName, start, interval, updateFn, stopAtZero)
@@ -64,7 +60,6 @@ end
 
     Uses Sleitnicks Timer module to create a ``Simple`` timer that counts up from 0 to ``endT``.
     Returns a signal that fires when the timer is finished.
-
     Timer is automatically disconnected at the end of the timer.
     
     @return Signal
@@ -98,4 +93,4 @@ function TimerCountdown:Destroy(name)
     end;
 end
 
-return TimerCountdown;
+return table.freeze(TimerCountdown);
